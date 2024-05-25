964a1439953e6b116709eb4de6681376a7ee8587
thrombash <nickel2730@ya.ru>
2024-05-25 21:38:21 +0400

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean == helloWorld);
+        System.out.println(cat1 == cat2);
+
     }
 }
